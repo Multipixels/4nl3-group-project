8dfd28bc2ea6f30939063070d31bda80786ba9fc
Multipixels <motorger@mcmaster.ca>
2025-03-17 11:32:00 -0400

    fix some stuff with majority

--- 3-competition-launch/codabench/starting_kit/Majority/model.py
+++ 3-competition-launch/codabench/starting_kit/Majority/model.py
@@ -4,23 +4,29 @@ import pandas as pd
 
 class Model:
     def __init__(self):
-        values = {}
+        self.values = {}
 
     # Train the model
     def fit(self, X, y):
-        values = {}
         for game in y:
-            for message in y[game]:
+            for message in game:
                 if pd.isna(message): break
-                if message["label"] in values: values[message["label"]] += 1
-                else: values[message["label"]] = 1
+                if message["label"] in self.values: self.values[message["label"]] += 1
+                else: self.values[message["label"]] = 1
         
 
     # Predict labels.
     def predict(self, X):
+        majority_label = 0
+        total = 0
+        for key in self.values:
+            if self.values[key] > total:
+                majority_label = key
+                total = self.values[key] 
+
         predictions = []
         for game in X:
             for message in game:
                 if pd.isna(message): break
-                predictions.append(self.majority_label)
+                predictions.append(majority_label)
         return predictions
